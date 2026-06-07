b85a2969d2dae7e1a44a4c464cbbbe0f67b844f6
Nassim Sahib <nassim.sahib775@gmail.com>
2026-06-07 14:53:14 +0200

    OrderBook header file implemented

---
+++ include/Orderbook.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <map>
+#include <unordered_map>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+#include "Usings.hpp"
+#include "Order.hpp"
+#include "OrderModify.hpp"
+#include "OrderbookLevelInfos.hpp"
+#include "Trade.hpp"
+
+class Orderbook
+{
+private:
+    struct OrderEntry
+    {
+        OrderPointer order_{ nullptr };
+        OrderPointers::iterator location_;
+    };
+
+    struct LevelData
+    {
+        Quantity quantity_{ };
+        Quantity count_ { };
+
+        enum class Action 
+        {
+            Add,
+            Remove, 
+            Match,
+        };
+    };
+
+    std::unordered_map<Price, LevelData> data_;
+    std::map<Price, OrderPointers, std::greater<Price>> bids_;
+    std::map<Price, OrderPointers, std::less<Price>> asks_;
+    std::unordered_map<OrderId, OrderEntry> orders_;
+    mutable std::mutex ordersMutex_;
+    std::thread ordersPruneThread_;
+    std::condition_variable shutdownConditionVariable_;
+    std::atomic<bool> shutdown_{ false };
+
+    void PruneGoodForDayOrders();
+
+    void CancelOrders(OrderIds orderIds);
+    void CancelOrderId(OrderId orderId);
+
+    void OnOrderCancelled(OrderPointer order);
+    void OnOrderAdded(OrderPointer order);
+    void OnOrderMatched(Price price, Quantity quantity, bool isFullyFilled);
+    void UpdateLevelData(Price price, Quantity quantity, LevelData::Action action);
+
+    bool CanFullyFill(Side side, Price price, Quantity quantity) const;
+    bool CanMatch(Side side, Price price) const;
+    Trades MatchOrders();
+
+public:
+    OrderBook();
+    OrderBook(const OrderBook&) = delete;
+    void operator=(const OrderBook&) = delete;
+    OrderBook(OrderBook&&) = delete;
+    void operator=(OrderBook&&) = delete;
+    ~OrderBook();
+
+    Trades AddOrder(OrderPointer order);
+    void CancelOrder(OrderId orderId);
+    Trades ModifyOrder(OrderModify order);
+
+    std::size_t Size() const;
+    OrderBookLevelInfos GetOrderInfos() const;
+
+};
